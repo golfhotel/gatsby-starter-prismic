a1da2c9f50561e6381d876055e8f957b28553fd5
golfhotel <webmaster@hosthello.com>
2020-11-05 20:39:32 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-prismic',
       options: {
-        repositoryName: 'gatsby',
+        repositoryName: 'foobarbazfaz',
         accessToken: `${process.env.API_KEY}`,
         linkResolver: () => post => `/${post.uid}`,
         htmlSerializer: () => (type, element, content) => {
